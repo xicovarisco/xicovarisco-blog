583150a3d63b1ae5e631c518c2692ebc75e5b6c1
Francisco Varisco <francisco.varisco@gmail.com>
2019-09-02 21:51:58 +1000

    Importing React-Google-MiniCalendar to create an example

---
+++ src/pages/react-google-minicalendar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DummyCalendarComponent from 'react-google-minicalendar';
+
+const Test = () => (
+    <>
+        <DummyCalendarComponent />
+    </>
+)
+
+export default Test;
